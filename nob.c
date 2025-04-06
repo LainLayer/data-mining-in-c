55683ae3d71d89b8063e01220f3e6da153282952
LainLayer <doggymangc@gmail.com>
2025-04-06 19:44:44 +0300

    [nob.c] Updated to use `NOB_STRIP_PREFIX`

--- nob.c
+++ nob.c
@@ -1,28 +1,29 @@
+#define NOB_STRIP_PREFIX
 #define NOB_IMPLEMENTATION
 #include "./src/nob.h"
 
-Nob_Proc build_program(const char *source_path, const char *output_path)
+Proc build_program(const char *source_path, const char *output_path)
 {
-    Nob_Cmd cmd = {0};
-    nob_cmd_append(&cmd, "cc");
-    nob_cmd_append(&cmd, "-Wall", "-Wextra", "-ggdb");
-    nob_cmd_append(&cmd, "-I./raylib/", "-I./zlib/", "-I./stb/");
-    nob_cmd_append(&cmd, "-O3");
-    nob_cmd_append(&cmd, "-o", output_path);
-    nob_cmd_append(&cmd, source_path);
-    nob_cmd_append(&cmd, "-L./raylib/", "-L./zlib/");
-    nob_cmd_append(&cmd, "-lraylib", "-lm", "-lz", "-ldl", "-lpthread");
-    return nob_cmd_run_async(cmd);
+    Cmd cmd = {0};
+    cmd_append(&cmd, "cc");
+    cmd_append(&cmd, "-Wall", "-Wextra", "-ggdb");
+    cmd_append(&cmd, "-I./raylib/", "-I./zlib/", "-I./stb/");
+    cmd_append(&cmd, "-O3");
+    cmd_append(&cmd, "-o", output_path);
+    cmd_append(&cmd, source_path);
+    cmd_append(&cmd, "-L./raylib/", "-L./zlib/");
+    cmd_append(&cmd, "-lraylib", "-lm", "-lz", "-ldl", "-lpthread");
+    return cmd_run_async(cmd);
 }
 
 int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
-    if (!nob_mkdir_if_not_exists("./build/")) return 1;
-    Nob_Procs procs = {0};
-    nob_da_append(&procs, build_program("./src/2d.c", "./build/2d"));
-    nob_da_append(&procs, build_program("./src/3d.c", "./build/3d"));
-    nob_da_append(&procs, build_program("./src/knn.c", "./build/knn"));
-    if (!nob_procs_wait(procs)) return 1;
+    if (!mkdir_if_not_exists("./build/")) return 1;
+    Procs procs = {0};
+    da_append(&procs, build_program("./src/2d.c", "./build/2d"));
+    da_append(&procs, build_program("./src/3d.c", "./build/3d"));
+    da_append(&procs, build_program("./src/knn.c", "./build/knn"));
+    if (!procs_wait(procs)) return 1;
     return 0;
 }
